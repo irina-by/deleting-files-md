d4302d4bca133886dd722f3279764edf5370d24b
Ira <irina.a.ananchenko@gmail.com>
2024-01-15 13:26:10 +0300

    добавила удаление файлов md

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Deleting_files_md
+{
+    partial class Удалить
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Удалить));
+            this.delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // delete
+            // 
+            this.delete.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            resources.ApplyResources(this.delete, "delete");
+            this.delete.Name = "delete";
+            this.delete.UseVisualStyleBackColor = false;
+            this.delete.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Удалить
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.Controls.Add(this.delete);
+            this.DoubleBuffered = true;
+            this.Name = "Удалить";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button delete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿// программа должна удалить все пустые файлы с раcширением.md внутри всех папок и подпапок где лежит сама программа.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Deleting_files_md
+{
+    public partial class Удалить : Form
+    {
+        private string currentDirectory;
+        private List<string> deletedFiles;
+
+        public Удалить()
+        {
+            InitializeComponent();
+            currentDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+            deletedFiles = new List<string>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Вы уверены, что хотите удалить все пустые файлы с расширением '.md'?", "Подтверждение удаления", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                DeleteEmptyFiles(currentDirectory);
+                MessageBox.Show("Удаление завершено.\n\nУдаленные файлы:\n" + string.Join("\n", deletedFiles), "Успешно", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void DeleteEmptyFiles(string directory)
+        {
+            foreach (string file in Directory.GetFiles(directory, "*.md"))
+            {
+                if (IsFileEmpty(file))
+                {
+                    File.Delete(file);
+                    deletedFiles.Add(file);
+                }
+            }
+
+            foreach (string subDirectory in Directory.GetDirectories(directory))
+            {
+                DeleteEmptyFiles(subDirectory);
+                if (Directory.GetFiles(subDirectory).Length == 0 && Directory.GetDirectories(subDirectory).Length == 0)
+                {
+                    Directory.Delete(subDirectory);
+                }
+            }
+        }
+
+        private bool IsFileEmpty(string filePath)
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+            return fileInfo.Length == 0;
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] mdFiles = Directory.GetFiles(currentDirectory, "*.md");
+           
+        }
+
+
+    }
+}
